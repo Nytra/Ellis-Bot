28718682334e26eb3aa6c0d985f47937b5b6d4de
Sam Scott <samueltscott@gmail.com>
2018-04-03 13:50:09 +0100

    a

---
+++ ellis-bot.py
@@ -0,0 +1,23 @@
+import discord
+
+TOKEN = "NDMwNzA2NzQ4ODUzMTkwNjU2.DaUJSw.1GOfezdHzVV5ARD1DRLpniLyZZw"
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!hello"):
+        msg = "Hello {0.author.mention}".format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("-----")
+
+client.run(TOKEN)
